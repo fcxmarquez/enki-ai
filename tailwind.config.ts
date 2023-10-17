de824844ca818cc299bd9f04056d061c7623718c
fcxmarquez <francisco.marquez.solt@gmail.com>
2023-10-17 10:29:36 -0600

    style: import font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,36 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        primary: "#000000",
+        secondary: {
+          white: "#FFFFFF",
+          gray: "#8A8A8A",
+          blue: "#007BFF",
+        },
+        accent: {
+          red: "#FF0000",
+          green: "#28A745",
+        },
+      },
+      fontFamily: {
+        primary: ["Inter", "sans-serif"],
+        secondary: ["Mono", "monospace"],
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
