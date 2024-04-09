869d4c74b9f1d6dc2ea600e31b6f5fde0841dd2b
fcxmarquez <francisco.marquez.solt@gmail.com>
2024-04-09 17:18:04 -0600

    feat: added button and sections to modalconfig

--- components/Modals/ChakraModals/Config/index.tsx
+++ components/Modals/ChakraModals/Config/index.tsx
@@ -1,4 +1,10 @@
-import { ModalHeader, ModalContent, ModalBody, ModalCloseButton } from "@chakra-ui/react";
+import {
+  ModalHeader,
+  ModalContent,
+  ModalBody,
+  ModalCloseButton,
+  Button,
+} from "@chakra-ui/react";
 import { InputConfig } from "@/components/inputs/InputConfig";
 
 export const ModalConfig = () => {
@@ -7,7 +13,7 @@ export const ModalConfig = () => {
       <ModalHeader>Settings</ModalHeader>
       <ModalCloseButton />
       <ModalBody>
-        <div className="config-section">
+        <div className="config-section border-b-2 pb-2">
           <h2>API Keys</h2>
           <p>
             <strong>Introduce your API keys here</strong>
@@ -22,8 +28,13 @@ export const ModalConfig = () => {
             id="antropicKey"
             placeholder="Enter your Antropic Key"
           />
+          <Button>Save</Button>
         </div>
       </ModalBody>
     </ModalContent>
   );
 };
+
+// Sections for the modal
+// 1. Apikeys
+// 2. Logout
