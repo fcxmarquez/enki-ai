271ee5fa5a4149b4b83b8eb643531542bfe445e0
fcxmarquez <francisco.marquez.solt@gmail.com>
2025-05-22 18:28:46 -0600

    build: drizzle config and envs configurations

---
+++ drizzle.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "drizzle-kit";
+import dotenv from "dotenv";
+
+dotenv.config({ path: ".env.local" });
+
+export default defineConfig({
+  schema: "./db/schema.ts",
+  out: "./db/migrations",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  },
+});
